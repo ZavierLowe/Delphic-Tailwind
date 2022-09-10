19c29a98d246e8e5694ae6a32e5acc52c96f98e1
Zavier <85642176+ZavierLowe@users.noreply.github.com>
2022-09-10 15:43:34 -0400

    Fixed Classes and Added Video

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+var menuCart = document.getElementById("menu-cart")
+var menuBackground = document.getElementById("cart-bg")
 
 
+openCart = () =>{
+   menuCart.classList.remove("right-[-450px]");
+   menuCart.classList.add('right-0')
+   menuBackground.classList.add('top-0','left-0','bottom-0','right-0','fixed','bg-black','opacity-70','z-[49]');
+    
+}
+
+closeCart = () =>{
+    menuCart.classList.remove("right-0");
+	menuCart.classList.add("right-[-450px]");
+    menuBackground.classList.remove(
+			"top-0",
+			"left-0",
+			"bottom-0",
+			"right-0",
+			"fixed",
+			"bg-black",
+			"opacity-70",
+			"z-[49]",
+		);
+
+}
+
+
+
+
+
+
+//Add a div with the outside classes
+//while the cart drawer is open
+// closeAll = () =>{
+//     // var tag = document.createElement('div')
+//     // tag.classList.add("h-screen w-screen fixed top-0 left-0");
+//     // menuCart.lastChild.appendChild(tag)
+
+//     // if (openCart === true){
+//     //     closeCart()
+
+//     // }
+//     // else{
+//     //     null
+//     // }
+
+//     menuCart.classList.remove("right-0");
+// 		menuCart.classList.add("right-[-450px]");
+        
+
+// }
 
